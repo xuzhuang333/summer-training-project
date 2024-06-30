0872abfe0188022e7c3aa1f6bcaf13db5a3a531b
xuzhuang333 <3302821496@qq.com>
2024-06-30 14:26:54 +0800

    完成了发布消息的后端接口设计。

---
+++ src/main/java/com/example/back/Controller/TeacherControl.java
@@ -0,0 +1,47 @@
+package com.example.back.Controller;
+
+import com.example.back.beans.JsonResult;
+import com.example.back.beans.NoticeForPost;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@Slf4j
+@Api(tags = "只由老师控制的功能接⼝")
+public class TeacherControl {
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @PostMapping("/postNotice")
+    @ApiOperation(value = "信息发布")
+    public JsonResult<String> postnotice(@RequestBody NoticeForPost noticeForPost){
+        JsonResult<String> result = new JsonResult<String>();
+        try {
+            jdbc.update("INSERT INTO notice(notice_title,content)VALUES(?,?)",noticeForPost.getTitle(),noticeForPost.getContent());
+            result.setResult("消息发布成功");
+            result.setCode(200);
+            log.info("消息已发布");
+
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            result.setResult("消息未发布成功");
+            result.setCode(201);
+            log.error("消息未发布");
+
+        }
+        return result;
+
+    }
+
+}
--- src/main/java/com/example/back/beans/JsonResult.java
+++ src/main/java/com/example/back/beans/JsonResult.java
@@ -5,7 +5,8 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "给前端返回数据时用的类，包含状态码code和数据result")
 public class JsonResult<T> {
-    @ApiModelProperty(value = "code状态码，登录时：200为成功，201为未激活，202为密码错误。查宿舍时：200为成功，201为寝室只有一个人，202为ID不存在或ID不是学生ID")
+    @ApiModelProperty(value = "code状态码，登录时：200为成功，201为未激活，202为密码错误。查宿舍时：200为成功，201为寝室只有一个人，202为ID不存在或ID不是学生ID," +
+            "查志愿时长时：200为成功，202为用户不存在，消息发布时：200为成功，201为发布失败")
     private int code;
     @ApiModelProperty(value = "result为结果信息")
     private T result;
