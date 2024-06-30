12e98d658eae4eeb8ff533672082aa5a2e7fd064
xuzhuang333 <3302821496@qq.com>
2024-06-30 20:51:22 +0800

    完成了查询课表的后端接口设计。

---
+++ src/main/java/com/example/back/beans/CourseTable.java
@@ -0,0 +1,71 @@
+package com.example.back.beans;
+
+public class CourseTable {
+    private String course_name;
+    private String course_room;
+    private String startDate;
+    private String week;
+    private int time;
+    private int duration;
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_room() {
+        return course_room;
+    }
+
+    public void setCourse_room(String course_room) {
+        this.course_room = course_room;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getWeek() {
+        return week;
+    }
+
+    public void setWeek(String week) {
+        this.week = week;
+    }
+
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseTable{" +
+                "course_name='" + course_name + '\'' +
+                ", course_room='" + course_room + '\'' +
+                ", time=" + time +
+                ", startDate='" + startDate + '\'' +
+                ", week='" + week + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
