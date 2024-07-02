1dc312f364e488f50575084354af1b91bb0e2c9e
MDK <2547975308@qq.com>
2024-07-02 16:13:19 +0800

    chu shi hua

---
+++ src/main/java/com/project/beans/Agreedata.java
@@ -0,0 +1,30 @@
+package com.project.beans;
+
+public class Agreedata {
+    private Integer submit;
+    private Integer id;
+
+    public Integer getSubmit() {
+        return submit;
+    }
+
+    public void setSubmit(Integer submit) {
+        this.submit = submit;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "agreedata{" +
+                "submit=" + submit +
+                ", id=" + id +
+                '}';
+    }
+}
---
+++ src/main/java/com/project/beans/JsonResult.java
@@ -0,0 +1,40 @@
+package com.project.beans;
+
+public class JsonResult {
+    private String code;
+    private Object result;
+    private String msg;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "JsonResult{" +
+                "code='" + code + '\'' +
+                ", result=" + result +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
