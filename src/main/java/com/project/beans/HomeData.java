6c622929fce9afc3bd4f97dea7fdc10427a6887b
MDK <2547975308@qq.com>
2024-07-04 14:00:08 +0800

    大屏

---
+++ src/main/java/com/project/beans/HomeData.java
@@ -0,0 +1,12 @@
+package com.project.beans;
+
+public class HomeData {
+    private String[] names;
+    private Integer[] values;
+
+    // Getter 和 Setter 方法
+    public String[] getNames() { return names; }
+    public void setNames(String[] names) { this.names = names; }
+    public Integer[] getValues() { return values; }
+    public void setValues(Integer[] values) { this.values = values; }
+}
