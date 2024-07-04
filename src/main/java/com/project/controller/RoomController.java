4a27ba1f76b67ac980fc6bafd8c76aa748e25b6a
MDK <2547975308@qq.com>
2024-07-04 19:38:47 +0800

    寝室

--- src/main/java/com/project/controller/RoomController.java
+++ src/main/java/com/project/controller/RoomController.java
@@ -90,14 +90,14 @@ public class RoomController {
     public JsonResult roomagreee(@RequestBody Agreedata agreedata){
         JsonResult res = new JsonResult();
         try {
-            if(agreedata.getSubmit()==1){
+            if("1".equals(agreedata.getSubmit())){
                 jdbc.update("update yonghu set dormitory_state = 0 where id = ?",agreedata.getId());
                 jdbc.update("delete from roomagree where studentid = ?",agreedata.getId());
                 res.setCode("200");
                 res.setMsg("审批成功");
                 return res;
             }else {
-                jdbc.update("update yonghu set dormitory_state = -1 where id = ?",agreedata.getId());
+                jdbc.update("update yonghu set dormitory_state = 1 where id = ?",agreedata.getId());
                 jdbc.update("delete from roomagree where studentid = ?",agreedata.getId());
                 res.setCode("200");
                 res.setMsg("拒批成功");
